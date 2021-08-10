3eb4f07d8f5041280f933ac5b285fbc24d227418
khasizadaj <javid.khasizada@gmail.com>
2021-08-10 23:56:16 +0200

    views and urls updated

--- core/views.py
+++ core/views.py
@@ -2,28 +2,59 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 # third party imports
+from rest_framework import mixins
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
 
 from .seriealizers import PostSerializer
 from .models import Blog
 
 
-class TestView(APIView):
+class PostView(
+        mixins.ListModelMixin,
+        mixins.CreateModelMixin,
+        generics.GenericAPIView):
+    serializer_class = PostSerializer
+    queryset = Blog.objects.all()
+
     def get(self, request, *args, **kwargs):
-        # Serializing many posts
-        qs = Blog.objects.all()
-        serializer = PostSerializer(qs, many=True)  # returns list of dicts
-
-        # Serializing only one post
-        # post = qs.last()
-        # serializer = PostSerializer(post)
-
-        return Response(serializer.data)
+        return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+        return self.create(request, *args, **kwargs)
+
+
+class PostCreateView(
+        mixins.ListModelMixin,
+        generics.CreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Blog.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class PostListCreateView(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    queryset = Blog.objects.all()
+
+
+# class TestView(APIView):
+#     def get(self, request, *args, **kwargs):
+#         # Serializing many posts
+#         qs = Blog.objects.all()
+#         serializer = PostSerializer(qs, many=True)  # returns list of dicts
+
+#         # Serializing only one post
+#         # post = qs.last()
+#         # serializer = PostSerializer(post)
+
+#         return Response(serializer.data)
+
+#     def post(self, request, *args, **kwargs):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors)
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import TestView
+from core.views import PostView, PostCreateView, PostListCreateView
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('', TestView.as_view(), name='test'),
+    path('', PostView.as_view(), name='test'),
+    path('create', PostCreateView.as_view(), name='create'),
+    path('list-create', PostListCreateView.as_view(), name='list-create'),
 ]
